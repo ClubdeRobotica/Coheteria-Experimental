a7e1247f7753684b1479fe5c4eb2f55198ea990f
lasofre <lasofre@gmail.com>
2019-03-30 15:17:09 -0300

    Version 1.0

--- telemetria-control/Estacion de Tierra/ET.py
+++ telemetria-control/Estacion de Tierra/ET.py
@@ -1,21 +1,25 @@
 # coding=utf-8
 import socket
+PUERTO=8050
+
 #instanciamos un objeto para trabajar con el socket
 ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Puerto y servidor que debe escuchar
-ser.bind(("", 8050))
+ser.bind(("",PUERTO))
 #Aceptamos conexiones entrantes con el metodo listen. Por parámetro las conexiones simutáneas.
 ser.listen(1)
 #Instanciamos un objeto cli (socket cliente) para recibir datos
-cli, addr = ser.accept()
+
 archivo = open("data.txt", "w")
+print "Servidor en: "+ socket.gethostbyname(socket.gethostname())+" puerto: "+str(PUERTO)
+print "Esperando conexion"
+cli, addr = ser.accept()
+print "Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1])
 while True:
 
     #Recibimos el mensaje, con el metodo recv recibimos datos. Por parametro la cantidad de bytes para recibir
     recibido = cli.recv(1024)
-
     #Si se reciben datos nos muestra la IP y el mensaje recibido
-    print "Recibo conexion de la IP: " + str(addr[0]) + " Puerto: " + str(addr[1])
     print recibido
     if recibido == "end":
         break
