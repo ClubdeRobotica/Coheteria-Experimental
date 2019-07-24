5e2772170e696caf967a5c4a679c40629156e283
gerontivero <gerbonti377@gmail.com>
2019-07-24 00:22:39 -0300

    Primer version probada con el soft

--- Motor/Banco de pruebas/Firmware/BancoPruebas/BancoPruebas.ino
+++ Motor/Banco de pruebas/Firmware/BancoPruebas/BancoPruebas.ino
@@ -25,7 +25,9 @@ const char *password = APPSK;
 const float CALIBRAR = 11.4;
 const float PATRON = 204.4;
 
-ESP8266WebServer server(80);
+//ESP8266WebServer server(8000);
+WiFiServer wifiServer(8000);
+int led = 0;
 
 
 HX711 scale;
@@ -34,27 +36,18 @@ int   BufferIdx = 0;
 char Buffer[10240];
 char DataBuffer[10000];
 
-void handleRoot() {  
-  sprintf(Buffer, "<meta http-equiv=\"refresh\" content=\"0.1\";><h1>%s</h1>", DataBuffer);   
-  memset(DataBuffer, 0, 1200);
-  BufferIdx=0;
-  server.send(200, "text/html", Buffer);
-}
-
 
 void setup() {
-  delay(1000);
   Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
   Serial.println();
   Serial.print("Configurando access point...");
   /* You can remove the password parameter if you want the AP to be open. */
   WiFi.softAP(ssid, password);
   IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
   Serial.println(myIP);
-  server.on("/", handleRoot);
-  server.begin();
-  Serial.println("HTTP server started");
+  wifiServer.begin();
   
   Serial.print("Inicializando la escala...");
 
@@ -92,25 +85,32 @@ void setup() {
   Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
             // by the SCALE parameter set with set_scale
 
-  Serial.println("Readings:");
+  Serial.println("Readings:");  
 }
 
-void loop() {
-  tiempo = millis();
-  lectura = -scale.get_units(3);
-  gramos = lectura*PATRON/CALIBRAR;
-  //Serial.print((tiempo / 1000), 1);
-  //Serial.print("\t ms\t");  
-  //Serial.print("\t| ");
-  //Serial.print(gramos, 1);
-  //Serial.print(" gramos\n");  
-  if(BufferIdx == 0)
-    Serial.print("\n");
-  sprintf(&DataBuffer[BufferIdx], "{\"T\":%.3f,\"grs\":%.2f}", tiempo/1000, gramos); 
-  BufferIdx = strlen(DataBuffer);    
-  scale.power_down();             // put the ADC in sleep mode
-  //delay(10);
-  Serial.print(DataBuffer);
-  scale.power_up();
-  server.handleClient();
+void loop() {  
+  WiFiClient cliente  = wifiServer.available();    
+  while (cliente.connected()) {
+    digitalWrite(LED_BUILTIN, LOW); 
+    tiempo = millis();
+    lectura = -scale.get_units(1);
+    gramos = lectura*PATRON/CALIBRAR;
+    sprintf(DataBuffer, "{\"T\":%.3f,\"grs\":%.2f}", tiempo/1000, gramos); 
+    scale.power_down();      
+    cliente.write(DataBuffer);    
+    scale.power_up();
+  }
+  if(cliente.connected()) 
+    cliente.stop();
+  else  
+    delay(1);
+  led++;
+  if(led < 100){
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+    digitalWrite(LED_BUILTIN, HIGH);
+  if(led > 1000){
+    led = 0;
+  }
 }
